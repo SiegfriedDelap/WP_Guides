e900279657380e7de675ca66429dfd069bdc2f82
Pablo <pavlo.laptiev@gmail.com>
2021-07-15 23:44:21 +0300

    customizable logotype was added

--- wp-content/themes/childhood/header.php
+++ wp-content/themes/childhood/header.php
@@ -1,11 +1,26 @@
+<!-- Вариант структуры логотипа классический
+<div class="header__logo">
+    <?php the_custom_logo(); ?>
+</div> -->
+
+<!-- 
+Вариант собственной структуры логотипа 
+<a href="<?php echo get_home_url(); ?>" class="header__logo">
+    <img src="<?php 
+        // получаем ссылку на логотип
+        $custom_logo__url = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' ); 
+        // выводим
+        echo $custom_logo__url[0]; 
+    ?>" alt="World" class="header__logo-img">
+</a> -->
+
 <header>
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-11 col-sm-12 col-md-12 col-lg-3">
-                        <a href="#" class="header__logo">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/svg/logo.svg" alt="Мир детства" class="header__logo-img">
-                            <div class="header__logo-text">Мир детства</div>
-                        </a>
+                        <div class="header__logo">
+                            <?php the_custom_logo(); ?>
+                        </div>
                     </div>
                     <div class="col-md-12 col-lg-9 offset-lg-0 col-xl-7 offset-xl-2 ">
                         <div class="header__contacts">
