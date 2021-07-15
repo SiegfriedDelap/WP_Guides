7872e46d2a57b170194a99175947a8d78ded1bbd
Pablo <pavlo.laptiev@gmail.com>
2021-07-15 19:30:42 +0300

    functions for getting scripts and styles and Livehack in doc

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -1,6 +1,5 @@
 <?php 
 //хук 1арг что ловим, 2арг доп функционал
-<<<<<<< HEAD
 add_action('wp_enqueue_scripts', 'childhood_styles');
 add_action('wp_enqueue_scripts', 'childhood_scripts');
 
@@ -21,16 +20,6 @@ function childhood_scripts() {
     wp_enqueue_script('childhood-scripts', get_template_directory_uri(  ) . '/assets/js/main.min.js', array(), null, true);
 };
 
-=======
-add_action('wp_enqueue_scripts', 'childhood_scripts');
-
-function childhood_scripts() {
-    //подключение стилей
-    //первый аргумент название стиля, второй путь указывайт на главный файл темы
-    wp_enqueue_style('childhood-style', get_stylesheet_directory_uri());
-    
-}
->>>>>>> 5c6803ebf0b1ce05bb994ff899794e65402cfcc5
 
 
 ?>
